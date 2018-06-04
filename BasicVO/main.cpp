2df1cafd64784456a67062bd430f7da987878162
zhangxiaoya <zhangxiaoya1989@gmail.com>
2018-06-04 09:52:52 +0800

    detect fast keypoints use opencv2

--- BasicVO/main.cpp
+++ BasicVO/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/features2d.hpp>
+
+using namespace std;
+using namespace cv;
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    Mat img = imread("/home/runisys/Desktop/data/KITTI/object/training/image_2/000000.png");
+    if (img.empty())
+    {
+        cout << "Open Image failed!" << endl;
+        return -1;
+    }
+    FastFeatureDetector detector(40);
+    vector<KeyPoint> keyPoints;
+    detector.detect(img, keyPoints);
+    drawKeypoints(img, keyPoints, img);
+
+    imshow("Detected KeyPoints", img);
+    waitKey(0);
     return 0;
 }
